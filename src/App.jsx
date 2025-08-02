58dd39ceffc9473227c4edbe2a6bcdcf9d2e0879
geek-nish <nish.nsh00@gmail.com>
2025-08-02 15:41:34 +0530

    Spelling error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Hero from './components/hero';
+import Hero from './components/Hero';
 import Problem from './components/Problem';
 import Solution from './components/Solution';
 import Testimonial from './components/Testimonial';
