e5ebd13ffb83ae5d4ff46ea17fe5723e284480d1
geek-nish <nish.nsh00@gmail.com>
2025-08-02 17:38:24 +0530

    changes to tally form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -66,7 +66,7 @@ const Contact = () => {
               <p className="mt-3 questrial-regular text-gray-500 dark:text-gray-400">
                 Got questions before booking? Drop us a line anytime.
               </p>
-              <a href="#tally-open=31o16l&tally-overlay=1&tally-emoji-text=👋&tally-emoji-animation=wave&tally-form-events-forwarding=1">
+              <a href="#tally-open=31o16l&tally-overlay=1&tally-form-events-forwarding=1">
                 <button
                   type="button"
                   className="py-3 mt-5 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-indigo-400 text-gray-800 shadow-2xs hover:bg-gray-50 focus:outline-none focus:bg-gray-50 questrial-regular"
